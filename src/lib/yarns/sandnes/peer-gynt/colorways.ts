d88e21d956956e36cadf8fe961408ef383e9b2e2
Thomas <jdvlpr@icloud.com>
2024-12-21 22:10:30 +0100

    added new yarn: Sandnes - Peer Gynt

---
+++ src/lib/yarns/sandnes/peer-gynt/colorways.ts
@@ -0,0 +1,242 @@
+import type { Colorway } from '$lib/types';
+
+const colorways: Colorway[] = [
+  {
+    source: {
+      name: 'sandnesgarn.no',
+      href: 'https://www.sandnesgarn.no/peer-gynt-1865',
+      accessed: '2024-12-21',
+    },
+    colors: [
+      {
+        hex: '#f6f4eb',
+        name: 'Hvit 1001',
+      },
+      {
+        hex: '#f3efe4',
+        name: 'Hvit 1002',
+      },
+      {
+        hex: '#ede3cd',
+        name: 'Natur 1012',
+      },
+      {
+        hex: '#bbb9b5',
+        name: 'Lys Gråmelert 1032',
+      },
+      {
+        hex: '#bfbab2',
+        name: 'Lys Gråmelert Løvfall Tweed 1035',
+      },
+      {
+        hex: '#939188',
+        name: 'Gråmelert 1042',
+      },
+      {
+        hex: '#dfd5be',
+        name: 'Natur M/Tutti Frutti Tweed1044',
+      },
+      {
+        hex: '#666563',
+        name: 'Mørk Gråmelert 1053',
+      },
+      {
+        hex: '#292929',
+        name: 'Koksmelert 1088',
+      },
+      {
+        hex: '#1b1b1b',
+        name: 'Svart 1099',
+      },
+      {
+        hex: '#c3c5c0',
+        name: 'Lys Gråmelert M/Blå Tweed 1502',
+      },
+      {
+        hex: '#e8cc70',
+        name: 'Solgul 2016',
+      },
+      {
+        hex: '#cfbfb1',
+        name: 'Marsipan 2321',
+      },
+      {
+        hex: '#eae2d0',
+        name: 'Natur Tweed 2523',
+      },
+      {
+        hex: '#643d1b',
+        name: 'Gyllenbrun 2564',
+      },
+      {
+        hex: '#aca097',
+        name: 'Gråbeige Melert 2650',
+      },
+      {
+        hex: '#726660',
+        name: 'Mellombrun Melert 2652',
+      },
+      {
+        hex: '#d0c2b3',
+        name: 'Marsipan M/Tutti Frutti Tweed 2720',
+      },
+      {
+        hex: '#c5b5a3',
+        name: 'Beigmelert Natur Tweed 2730',
+      },
+      {
+        hex: '#a75b2c',
+        name: 'Cognac 2745',
+      },
+      {
+        hex: '#c6b6a5',
+        name: 'Lys Beigemelert 3021',
+      },
+      {
+        hex: '#3f3130',
+        name: 'Mørk Brun 3082',
+      },
+      {
+        hex: '#665149',
+        name: 'Eikenøtt 3161',
+      },
+      {
+        hex: '#f2d6c9',
+        name: 'Ballet Tutu 3509',
+      },
+      {
+        hex: '#2e2b2c',
+        name: 'Bristol Black 3800',
+      },
+      {
+        hex: '#d3472c',
+        name: 'Spicy Orange 3819',
+      },
+      {
+        hex: '#7d413c',
+        name: 'Spiced Apple 3854',
+      },
+      {
+        hex: '#b82f2a',
+        name: 'Scarlet Red4018',
+      },
+      {
+        hex: '#eeb1ac',
+        name: 'Blossom 4213',
+      },
+      {
+        hex: '#7c1824',
+        name: 'Rød 4228',
+      },
+      {
+        hex: '#822139',
+        name: 'Rumba Red 4255',
+      },
+      {
+        hex: '#e88c99',
+        name: 'Bubblegum Pink 4315',
+      },
+      {
+        hex: '#5c3543',
+        name: 'Vinrød 4363',
+      },
+      {
+        hex: '#982d5d',
+        name: 'Jazzy Pink 4600',
+      },
+      {
+        hex: '#e594b9',
+        name: 'Shocking Pink 4626',
+      },
+      {
+        hex: '#ae577e',
+        name: 'Magenta 4628',
+      },
+      {
+        hex: '#4e2f48',
+        name: 'Bjørnebærsaft 4672',
+      },
+      {
+        hex: '#c6b1c5',
+        name: 'Lilac5023',
+      },
+      {
+        hex: '#21233b',
+        name: 'Marine 5575',
+      },
+      {
+        hex: '#d0d9db',
+        name: 'Arctic Ice 5811',
+      },
+      {
+        hex: '#2d4c95',
+        name: 'Dazzling Blue 5845',
+      },
+      {
+        hex: '#5475a3',
+        name: 'Regatta Blå 6044',
+      },
+      {
+        hex: '#1e4886',
+        name: 'Jolly Blue 6046',
+      },
+      {
+        hex: '#1c2e46',
+        name: 'Mørk Blå 6062',
+      },
+      {
+        hex: '#6e829b',
+        name: 'Blåmelert 6324',
+      },
+      {
+        hex: '#2d3250',
+        name: 'Mørk Blå 6364',
+      },
+      {
+        hex: '#4a5b69',
+        name: 'Mørk Blå 6572',
+      },
+      {
+        hex: '#2e2d35',
+        name: 'Mørk Gråblå 6581',
+      },
+      {
+        hex: '#2a4041',
+        name: 'Dyp Petrol 7281',
+      },
+      {
+        hex: '#7d8981',
+        name: 'Eukalyptus 8051',
+      },
+      {
+        hex: '#2f402f',
+        name: 'Skoggrønn 8082',
+      },
+      {
+        hex: '#45915e',
+        name: 'Jelly Bean Green 8236',
+      },
+      {
+        hex: '#a0b786',
+        name: 'Spring Green 8733',
+      },
+      {
+        hex: '#efd97b',
+        name: 'Lemon9004',
+      },
+      {
+        hex: '#444a48',
+        name: 'Urban Chic 9080',
+      },
+      {
+        hex: '#4a4f3b',
+        name: 'Mørk Grønnmelert 9572',
+      },
+      {
+        hex: '#69593b',
+        name: 'Dark Olive 9873',
+      },
+    ],
+  },
+];
+export default colorways;
