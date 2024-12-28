c3c5906365633971c780189042ea3b800e42e93c
Thomas <jdvlpr@icloud.com>
2024-12-28 17:18:15 +0100

    migrated sort palette component to runes mode

--- src/lib/components/modals/SortPalette.svelte
+++ src/lib/components/modals/SortPalette.svelte
@@ -13,108 +13,113 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with Temperature-Blanket-Web-App. 
 If not, see <https://www.gnu.org/licenses/>. -->
 
-<script>
+<script lang="ts">
   import ColorPaletteEditable from '$lib/components/ColorPaletteEditable.svelte';
   import SaveAndCloseButtons from '$lib/components/modals/SaveAndCloseButtons.svelte';
   import StickyPart from '$lib/components/modals/StickyPart.svelte';
   import { getSortedPalette } from '$lib/utils';
-  import { getContext } from 'svelte';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+  import ModalShell from './ModalShell.svelte';
 
-  export let colors, updateGauge;
+  let { colors, updateGauge, parent } = $props();
 
-  let sortColors = 'custom';
+  let _colors = $state(colors);
 
-  const { close } = getContext('simple-modal');
+  let sortColors = $state('custom');
+
+  const modalStore = getModalStore();
 </script>
 
-<div class="p-2 sm:p-4 w-[98vw] md:w-[700px]">
-  <div class="flex flex-col gap-2 items-center mt-6 sm:mt-4">
-    <div class="flex flex-wrap gap-2 justify-center items-end">
-      <label class="label">
-        <span class="flex items-center"
-          ><svg
+<ModalShell {parent}>
+    <div class="flex flex-col gap-2 items-center mt-6 sm:mt-4">
+      <div class="flex flex-wrap gap-2 justify-center items-end">
+        <label class="label">
+          <span class="flex items-center"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4 mr-1"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+              />
+            </svg>
+            Sort By</span
+          >
+          <select
+            class="select truncate"
+            id="sort-colors-by"
+            bind:value={sortColors}
+            onchange={() => {
+              _colors = getSortedPalette({
+                palette: _colors,
+                sortColors,
+              });
+            }}
+          >
+            <option value="custom">Custom</option>
+            <option value="light-to-dark">Lightest to Darkest</option>
+            <option value="dark-to-light">Darkest to Lightest</option>
+          </select>
+        </label>
+  
+        <button
+          class="btn bg-secondary-hover-token"
+          onclick={() => {
+            sortColors = 'custom';            
+            _colors.reverse();
+          }}
+          title="Reverse Colors' Positions"
+        >
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-4 h-4 mr-1"
+            class="w-6 h-6 mr-1"
           >
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+              d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
             />
           </svg>
-          Sort By</span
-        >
-        <select
-          class="select truncate"
-          id="sort-colors-by"
-          bind:value={sortColors}
-          on:change={() => {
-            colors = getSortedPalette({
-              palette: colors,
-              sortColors,
-            });
+          Reverse
+        </button>
+      </div>
+      <div class="w-full">
+        <ColorPaletteEditable
+          on:changed={() => {
+            sortColors = 'custom';
           }}
-        >
-          <option value="custom">Custom</option>
-          <option value="light-to-dark">Lightest to Darkest</option>
-          <option value="dark-to-light">Darkest to Lightest</option>
-        </select>
-      </label>
-
-      <button
-        class="btn bg-secondary-hover-token"
-        on:click={() => {
-          sortColors = 'custom';
-          colors = colors.reverse();
-        }}
-        title="Reverse Colors' Positions"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
-          />
-        </svg>
-        Reverse
-      </button>
+          canUserEditColor={false}
+          bind:colors={_colors}
+        />
+      </div>
     </div>
-    <div class="w-full">
-      <ColorPaletteEditable
-        on:changed={() => {
-          sortColors = 'custom';
-        }}
-        canUserEditColor={false}
-        bind:colors
-      />
-    </div>
-  </div>
-</div>
-
-<StickyPart position="bottom">
-  <div class="p-2 sm:p-4">
-    <SaveAndCloseButtons
-      onSave={() => {
-        updateGauge({
-          _colors: colors.map((n) => {
-            delete n.id;
-            return n;
-          }),
-        });
-        close();
-      }}
-      onClose={close}
-    />
-  </div>
-</StickyPart>
+  
+  {#snippet stickyPart()}
+    <StickyPart position="bottom">
+      <div class="p-2">
+        <SaveAndCloseButtons
+          onSave={() => {
+            updateGauge({
+              _colors: _colors.map((n) => {
+                delete n.id;
+                return n;
+              }),
+            });
+            modalStore.close();
+          }}
+          onClose={modalStore.close}
+        />
+      </div>
+    </StickyPart>
+  {/snippet}
+</ModalShell>
