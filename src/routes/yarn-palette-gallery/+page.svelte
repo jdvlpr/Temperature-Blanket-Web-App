305035650619ab993f9273bf40d1649c17945723
Thomas <jdvlpr@icloud.com>
2025-01-23 09:02:00 +0100

    fixed scroll issue

--- src/routes/yarn-palette-gallery/+page.svelte
+++ src/routes/yarn-palette-gallery/+page.svelte
@@ -105,11 +105,8 @@ If not, see <https://www.gnu.org/licenses/>. -->
     const scrollObserver = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach((entry) => {
-          if (entry.intersectionRatio === 0) {
-            showScrollToTopButton = true;
-          } else {
-            showScrollToTopButton = false;
-          }
+          showScrollToTopButton =
+            !entry.isIntersecting && entry.boundingClientRect.top < 0;
         });
       },
       { threshold: 0 },
