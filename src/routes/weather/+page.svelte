00652870b529621d4e2a51f95a7b1c8b7fff583d
Thomas <jdvlpr@icloud.com>
2025-02-25 17:44:01 +0100

    modified padding

--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -519,7 +519,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
                       }).icon}
                     </div>
                     <p
-                      class="text-8xl flex items-start justify-center font-light"
+                      class="text-8xl flex items-start justify-center font-bold"
                     >
                       {weatherData.current_weather.temperature}°
                     </p>
