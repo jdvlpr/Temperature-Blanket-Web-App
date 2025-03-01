e5f90639289492d405bd8db3fc64396b7b8a9302
Thomas <jdvlpr@icloud.com>
2025-03-01 12:56:41 +0100

    added yarn: Ice Yarns - Classic DK

--- src/lib/yarns/ice-yarns/yarns.ts
+++ src/lib/yarns/ice-yarns/yarns.ts
@@ -1,8 +1,9 @@
 import type { Brand } from '$lib/types';
+import { yarn as classicDK } from './classic-dk/yarn';
 import { yarn as lorenaWorsted } from './lorena-worsted/yarn';
 
 export const brand: Brand = {
   name: 'Ice Yarns',
   id: 'ice_yarns',
-  yarns: [lorenaWorsted],
+  yarns: [classicDK, lorenaWorsted],
 };
