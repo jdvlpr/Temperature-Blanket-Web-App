e4f0cadb0314cfe7c9d775fa02deb7224848b14b
Thomas <jdvlpr@icloud.com>
2025-03-15 11:15:30 +0100

    updated icon

--- src/lib/components/Locations.svelte
+++ src/lib/components/Locations.svelte
@@ -30,7 +30,12 @@ If not, see <https://www.gnu.org/licenses/>. -->
   import { slide } from 'svelte/transition';
   import SearchForWeather from './buttons/SearchForWeather.svelte';
   import {
+    CheckCheckIcon,
+    CheckIcon,
+    CircleCheckBigIcon,
+    CircleCheckIcon,
     CirclePlusIcon,
+    SquareCheckBigIcon,
     TriangleAlertIcon,
     WrenchIcon,
   } from '@lucide/svelte';
@@ -41,18 +46,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
     <p
       class="flex w-full flex-wrap items-center justify-center gap-1 text-center text-sm"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        class="h-5 w-5"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <CircleCheckBigIcon class=" size-4" />
       Loaded project and {#if weather.isUserEdited}custom weather{:else}weather{/if}
       data
     </p>
@@ -60,18 +54,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
     <p
       class="flex w-full flex-wrap items-center justify-center gap-1 text-center text-sm"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        class="h-5 w-5"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <CircleCheckBigIcon class=" size-4" />
       Loaded project
     </p>
   {/if}
