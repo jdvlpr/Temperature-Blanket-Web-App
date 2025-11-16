5a9e4374fb5ad08acceab1f3befc140261d4ffbc
Thomas <jdvlpr@icloud.com>
2025-11-16 16:02:55 +0100

    fixed layout issues by changing animation

--- src/lib/components/modals/DialogProvider.svelte
+++ src/lib/components/modals/DialogProvider.svelte
@@ -16,7 +16,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
 <script lang="ts">
   import { allGaugesAttributes, dialog, gauges, weather } from '$lib/state';
   import { Dialog, Portal } from '@skeletonlabs/skeleton-svelte';
-  import { scale } from 'svelte/transition';
+  import { fly } from 'svelte/transition';
   import CloseButton from './CloseButton.svelte';
   import SaveAndCloseButtons from './SaveAndCloseButtons.svelte';
 </script>
@@ -39,7 +39,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
       >
         {#snippet element(attributes)}
           {#if !attributes.hidden}
-            <div {...attributes} in:scale={{ duration: 150 }}>
+            <div {...attributes} in:fly={{ y: 50, duration: 150 }}>
               {#if dialog.type === 'component'}
                 {#if dialog.options.showCloseButton}
                   <Dialog.CloseTrigger
