b9098d4bb0e26c758d25c0296d99d9a783764e50
Thomas <jdvlpr@icloud.com>
2025-11-17 08:19:18 +0100

    updated gauge layout

--- src/lib/components/GaugeCustomizer.svelte
+++ src/lib/components/GaugeCustomizer.svelte
@@ -53,8 +53,12 @@ If not, see <https://www.gnu.org/licenses/>. -->
   let numberOfColumns = $derived.by(() => {
     let cols = 4;
     if (hasAnyAffiliateURLs) cols++;
-    if (showDaysInRange.value) cols++;
     if (sortableColors.length < 2) cols--;
+    if (!isProjectPlannerPage) {
+      cols--;
+      return cols;
+    }
+    if (showDaysInRange.value) cols++;
     return cols;
   });
 
@@ -97,6 +101,8 @@ If not, see <https://www.gnu.org/licenses/>. -->
     sortableColors = getSortableColors();
     dialog.close();
   }
+
+  $inspect(numberOfColumns);
 
   function handleConsider(e) {
     dragDisabled = true;
@@ -154,7 +160,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
 <div class="grid grid-cols-12 gap-2 pt-2">
   {#if isProjectPlannerPage}
     <div
-      class="col-span-full flex w-fit flex-col items-start gap-1 text-left md:col-span-8"
+      class="col-span-full flex w-fit flex-col items-start gap-1 text-left lg:col-span-8"
     >
       <ToggleSwitch
         bind:checked={showDaysInRange.value}
@@ -166,7 +172,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
 
   <div
     class="col-span-full my-2 flex flex-wrap justify-center {isProjectPlannerPage
-      ? 'md:cols-start-9 md:col-span-4 md:justify-end'
+      ? 'lg:cols-start-9 lg:col-span-4 lg:justify-end'
       : ''}"
   >
     <ViewToggle />
@@ -176,7 +182,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
 <div
   class="rounded-container mt-2 mb-2 overflow-hidden lg:mb-4 {localState.value
     .layout === 'grid'
-    ? 'grid grid-cols-2 gap-1 md:grid-cols-3 xl:grid-cols-4'
+    ? 'grid grid-cols-2 gap-1 lg:grid-cols-3 xl:grid-cols-4'
     : 'flex flex-col'}"
   use:dragHandleZone={{
     items: sortableColors,
@@ -189,127 +195,148 @@ If not, see <https://www.gnu.org/licenses/>. -->
 >
   {#each sortableColors as { hex, name, brandId, yarnId, brandName, yarnName, variant_href, affiliate_variant_href, id }, index (id)}
     <div
-      class="color flex flex-wrap items-center gap-2 p-2 {localState.value
-        .layout === 'grid'
-        ? 'rounded-container flex-auto basis-1/3 justify-around sm:basis-1/4 md:basis-1/5'
-        : 'justify-around'}"
+      class="color flex flex-wrap items-center justify-around gap-2 p-2 {localState
+        .value.layout === 'grid'
+        ? 'rounded-container flex-auto basis-1/3  sm:basis-1/4 md:basis-1/5'
+        : `${isProjectPlannerPage ? numberOfColumns < 6 && 'lg:grid lg:grid-cols-[1fr_3fr_1fr]' : numberOfColumns === 4 ? 'lg:grid lg:grid-cols-[1fr_3fr_1fr]' : 'lg:grid lg:grid-cols-[1fr_1.4fr_1fr]'}`}"
       style="background:{hex};color:{getTextColor(hex)}"
       animate:flip={{ duration: flipDurationMs }}
     >
-      <div class="flex flex-wrap items-center gap-2">
-        {#if movable && !isStaticGauge}
+      <div
+        class={[
+          'hidden',
+          localState.value.layout === 'list' &&
+            'lg:block lg:h-full lg:w-full lg:flex-auto',
+        ]}
+      ></div>
+      <div
+        class={[
+          localState.value.layout === 'list' &&
+            'flex flex-auto flex-wrap items-center justify-around gap-2',
+        ]}
+      >
+        <div class="flex flex-wrap items-center gap-2">
+          {#if movable && !isStaticGauge}
+            <button
+              title="Remove Color"
+              class="btn hover:preset-tonal flex flex-wrap items-center justify-center"
+              onclick={() => {
+                gauge.updateColors({
+                  colors: gauge.colors.filter((_, i) => i !== index),
+                });
+                sortableColors = getSortableColors();
+                gauge.schemeId = 'Custom';
+              }}
+            >
+              <span class="text-xs">{index + 1}</span>
+              <Trash2Icon />
+            </button>
+          {/if}
+
           <button
-            title="Remove Color"
-            class="btn hover:preset-tonal flex flex-wrap items-center justify-center"
-            onclick={() => {
-              gauge.updateColors({
-                colors: gauge.colors.filter((_, i) => i !== index),
-              });
-              sortableColors = getSortableColors();
-              gauge.schemeId = 'Custom';
-            }}
+            title="Move Color"
+            tabindex="-1"
+            aria-label="Crag handle for color {index + 1}"
+            class="btn-icon hover:preset-tonal handle p-2 {dragDisabled
+              ? 'cursor-grabbing'
+              : 'cursor-grab'}"
+            onmousedown={startDrag}
+            ontouchstart={startDrag}
+            use:dragHandle
           >
-            <span class="text-xs">{index + 1}</span>
-            <Trash2Icon />
+            <MoveIcon />
           </button>
-        {/if}
-
-        <button
-          title="Move Color"
-          tabindex="-1"
-          aria-label="Crag handle for color {index + 1}"
-          class="btn-icon hover:preset-tonal handle p-2 {dragDisabled
-            ? 'cursor-grabbing'
-            : 'cursor-grab'}"
-          onmousedown={startDrag}
-          ontouchstart={startDrag}
-          use:dragHandle
-        >
-          <MoveIcon />
-        </button>
-      </div>
-
-      {#if isProjectPlannerPage}
-        <div
-          class={['flex gap-2', localState.value.layout === 'grid' ? '' : '']}
-        >
-          {#key index}
-            <ColorRange {index} />
-          {/key}
         </div>
-      {/if}
 
-      <div class={localState.value.layout === 'grid' ? '' : 'flex-auto'}>
-        <button
-          class={['btn hover:preset-tonal flex h-auto justify-start']}
-          title="Choose a Color"
-          onclick={() =>
-            dialog.trigger({
-              type: 'component',
-              component: {
-                ref: ChangeColor,
-                props: {
-                  index,
-                  hex,
-                  name,
-                  brandId,
-                  yarnId,
-                  brandName,
-                  yarnName,
-                  variant_href,
-                  affiliate_variant_href,
-                  onChangeColor,
-                },
-              },
-              options: {
-                size: 'medium',
-              },
-            })}
-        >
-          <SearchIcon />
-          <span
-            class="flex flex-col items-start justify-start text-left text-wrap"
-          >
-            <span class="text-xs">
-              {#if brandName && yarnName}
-                {brandName}
-                -
-                {yarnName}
-              {:else}
-                Find Matching Yarn
-              {/if}
-            </span>
-            <span class="text-lg leading-tight"> {name || hex}</span>
-          </span>
-        </button>
-      </div>
-
-      {#if affiliate_variant_href}
-        <a
-          class="btn hover:preset-tonal"
-          href={affiliate_variant_href}
-          target="_blank"
-          rel="noreferrer nofollow"
-        >
-          <ShoppingBagIcon />
-          <span class="underline">Buy</span>
-        </a>
-      {/if}
-
-      {#if isProjectPlannerPage}
-        {#if showDaysInRange.value}
+        {#if isProjectPlannerPage}
           <div
-            class="bg-surface-900/10 rounded-container flex w-fit flex-wrap items-center justify-center overflow-hidden shadow-inner"
+            class={['flex gap-2', localState.value.layout === 'grid' ? '' : '']}
           >
-            <DaysInRange
-              range={gauge.ranges[index]}
-              rangeOptions={gauge?.rangeOptions}
-              targets={gauge.targets}
-              gaugeUnitType={gauge.unit.type}
-            />
+            {#key index}
+              <ColorRange {index} />
+            {/key}
           </div>
         {/if}
-      {/if}
+
+        <div class={[localState.value.layout === 'list' && 'flex-auto']}>
+          <button
+            class={['btn hover:preset-tonal flex h-auto justify-start']}
+            title="Choose a Color"
+            onclick={() =>
+              dialog.trigger({
+                type: 'component',
+                component: {
+                  ref: ChangeColor,
+                  props: {
+                    index,
+                    hex,
+                    name,
+                    brandId,
+                    yarnId,
+                    brandName,
+                    yarnName,
+                    variant_href,
+                    affiliate_variant_href,
+                    onChangeColor,
+                  },
+                },
+                options: {
+                  size: 'medium',
+                },
+              })}
+          >
+            <SearchIcon />
+            <span
+              class="flex flex-col items-start justify-start text-left text-wrap"
+            >
+              <span class="text-xs">
+                {#if brandName && yarnName}
+                  {brandName}
+                  -
+                  {yarnName}
+                {:else}
+                  Find Matching Yarn
+                {/if}
+              </span>
+              <span class="text-lg leading-tight"> {name || hex}</span>
+            </span>
+          </button>
+        </div>
+
+        {#if affiliate_variant_href}
+          <a
+            class="btn hover:preset-tonal"
+            href={affiliate_variant_href}
+            target="_blank"
+            rel="noreferrer nofollow"
+          >
+            <ShoppingBagIcon />
+            <span class="underline">Buy</span>
+          </a>
+        {/if}
+
+        {#if isProjectPlannerPage}
+          {#if showDaysInRange.value}
+            <div
+              class="bg-surface-900/10 rounded-container flex w-fit flex-wrap items-center justify-center overflow-hidden shadow-inner"
+            >
+              <DaysInRange
+                range={gauge.ranges[index]}
+                rangeOptions={gauge?.rangeOptions}
+                targets={gauge.targets}
+                gaugeUnitType={gauge.unit.type}
+              />
+            </div>
+          {/if}
+        {/if}
+      </div>
+      <div
+        class={[
+          'hidden',
+          localState.value.layout === 'list' &&
+            'lg:block lg:h-full lg:w-full lg:flex-auto',
+        ]}
+      ></div>
     </div>
   {/each}
 </div>
