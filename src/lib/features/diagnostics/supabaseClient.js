8b972e5bc90975d48edf3c2b294ac152b4232a39
Thomas <jdvlpr@icloud.com>
2026-01-17 06:35:55 +0100

    chore: Disable Supabase client functionality, remove its dependency, and update various development packages.

--- src/lib/features/diagnostics/supabaseClient.js
+++ src/lib/features/diagnostics/supabaseClient.js
@@ -1,12 +1,13 @@
-import {
-  PUBLIC_SUPABASE_ANON_KEY,
-  PUBLIC_SUPABASE_PROJECT_URL,
-} from '$env/static/public';
-import { createClient } from '@supabase/supabase-js';
+// // To use supabase, uncomment all of this file and add the following to the package.json dependencies: "@supabase/supabase-js": "^2.90.0",
+// import {
+//   PUBLIC_SUPABASE_ANON_KEY,
+//   PUBLIC_SUPABASE_PROJECT_URL,
+// } from '$env/static/public';
+// import { createClient } from '@supabase/supabase-js';
 
-// Supabase client instance
-// This is only used when collecting debugging diagnostic data
-export const supabase = createClient(
-  PUBLIC_SUPABASE_PROJECT_URL,
-  PUBLIC_SUPABASE_ANON_KEY,
-);
+// // Supabase client instance
+// // This is only used when collecting debugging diagnostic data
+// export const supabase = createClient(
+//   PUBLIC_SUPABASE_PROJECT_URL,
+//   PUBLIC_SUPABASE_ANON_KEY,
+// );
