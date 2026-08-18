24806138043d246be28a2df13bb835aa7b278b04
Thomas <jdvlpr@icloud.com>
2026-08-18 07:18:50 +0200

    performace enhancements - lazy load previews

--- src/lib/components/previews/PreviewSelect.svelte
+++ src/lib/components/previews/PreviewSelect.svelte
@@ -3,14 +3,14 @@
 This file is part of Temperature-Blanket-Web-App.
 
 Temperature-Blanket-Web-App is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the Free Software Foundation, 
+under the terms of the GNU General Public License as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
 
-Temperature-Blanket-Web-App is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+Temperature-Blanket-Web-App is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Temperature-Blanket-Web-App. 
+You should have received a copy of the GNU General Public License along with Temperature-Blanket-Web-App.
 If not, see <https://www.gnu.org/licenses/>. -->
 
 <script lang="ts">
@@ -97,7 +97,7 @@ If not, see <https://www.gnu.org/licenses/>. -->
       >
         {#each previews.all as { name, id }}
           {#if id === 'twsr'}
-            <option value={id}>{name} (New)</option>
+            <option value={id}>{name}</option>
           {:else}
             <option value={id}>{name}</option>
           {/if}
